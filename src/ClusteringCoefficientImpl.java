930c511e56a11d8f1991ac89caccfd2772c48630
Blazingwing <minhg.tw@yahoo.com.tw>
2015-11-24 19:27:58 +0800

    v.1.3a

---
+++ src/ClusteringCoefficientImpl.java
@@ -0,0 +1,46 @@
+
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.cytoscape.model.CyEdge;
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNode;
+
+public class ClusteringCoefficientImpl {
+
+	public double getClusteringCoefficient(CyNetwork network, CyNode node,
+			boolean directed) {
+
+		List<CyNode> neighborList;
+		neighborList = network.getNeighborList(node, CyEdge.Type.ANY);
+		double edgeCount = 0.0;
+		Set<CyEdge> edgeSet = new HashSet<CyEdge>();
+		for (CyNode neighbor1 : neighborList) {
+
+			for (CyNode neighbor2 : neighborList) {
+				if (neighbor1 == neighbor2)
+					continue;
+				if (directed) {
+					for (CyEdge edge : network.getConnectingEdgeList(neighbor1,
+							neighbor2, CyEdge.Type.ANY)) {
+						edgeSet.add(edge);
+					}
+				} else {
+					for (CyEdge edge : network.getConnectingEdgeList(neighbor1,
+							neighbor2, CyEdge.Type.ANY)) {
+						edgeCount++;
+						break;
+					}
+				}
+			}
+		}
+		if (directed)
+			return ((double) edgeSet.size() / (neighborList.size() * (neighborList
+					.size() - 1)));
+		else
+			return edgeCount
+					/ (neighborList.size() * (neighborList.size() - 1));
+	}
+}
--- src/MenuAction.java
+++ src/MenuAction.java
@@ -1,9 +1,16 @@
 
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import java.awt.Color;
 import java.awt.Component;
+import java.awt.Event;
 import java.awt.Shape;
 import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import javax.naming.directory.BasicAttribute;
@@ -17,8 +24,10 @@ import java.awt.image.SinglePixelPackedSampleModel;
 import org.cytoscape.app.CyAppAdapter;
 import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.application.swing.AbstractCyAction;
+import org.cytoscape.model.CyEdge;
 import org.cytoscape.model.CyNetwork;
 import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyTable;
 import org.cytoscape.model.CyTableUtil;
 import org.cytoscape.view.layout.CyLayoutAlgorithm;
 import org.cytoscape.view.layout.CyLayoutAlgorithmManager;
@@ -34,6 +43,7 @@ import org.cytoscape.application.swing.CySwingApplication;
 import org.cytoscape.model.CyNetworkManager;
 import org.cytoscape.application.swing.CytoPanelComponent;
 import org.cytoscape.application.swing.CyAction;
+import org.cytoscape.application.swing.CyEdgeViewContextMenuFactory;
 import org.cytoscape.service.util.AbstractCyActivator;
 
 import org.osgi.framework.BundleContext;
@@ -57,7 +67,49 @@ public class MenuAction extends AbstractCyAction {
         final CyApplicationManager manager = adapter.getCyApplicationManager();
         final CyNetworkView networkView = manager.getCurrentNetworkView();
         final CyNetwork network = manager.getCurrentNetwork();
-      
+
+        String line,tempstring;
+        String[] tempArray= new String[3];
+        ArrayList<?> myList = new ArrayList<Object>();
+        
+        //-----Term-----
+        
+/*    	FileReader fr = null;
+    	BufferedReader br = new BufferedReader(fr);
+		try {
+			fr = new FileReader("term.txt");
+			
+			
+			try {
+				while (br.ready()) {        	
+				    System.out.println(br.readLine()); 
+				
+				
+				    
+				    
+				    
+				    
+				fr.close();
+				}
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}*/
+    	
+
+    	
+        
+
+        	
+
+     
+        List<CyNode> neighborList;
+		
         //CyNode centroid = network.addNode();
                 
         //network.getRow(centroid).set(CyNetwork.NAME, "Centroid");
@@ -74,8 +126,9 @@ public class MenuAction extends AbstractCyAction {
         double count2 = 0; 
         double count3 = 0;
         double r = 15;
-        double sc = 0;
-
+        double m = 0;
+        double temp = 0;
+        int layer = 0;
         
         for (CyNode node : CyTableUtil.getNodesInState(network, "selected", true)){
         	nodeView = networkView.getNodeView(node);
@@ -95,33 +148,48 @@ public class MenuAction extends AbstractCyAction {
         double a = x/count3;
         double b = y/count3;
 
-        double temp = 0;
+        
+        
+        
+        
+        layer = (int)Math.sqrt((double)count1);
+        layer /= 2;
+        temp = 0;
         
         for (CyNode node : CyTableUtil.getNodesInState(network, "selected", true)){
         	nodeView = networkView.getNodeView(node);
+        	neighborList = network.getNeighborList(node, CyEdge.Type.ANY);
+        	
+        	m = (temp*360/count1)*Math.PI/180;
         	nodeView.setVisualProperty(BasicVisualLexicon.NODE_X_LOCATION, (r*(count1-1))*Math.cos((temp*360/count1)*Math.PI/180)+a);
         	nodeView.setVisualProperty(BasicVisualLexicon.NODE_Y_LOCATION, (r*(count1-1))*Math.sin((temp*360/count1)*Math.PI/180)+b);
         	
+        	nodeView.setLockedValue(BasicVisualLexicon.NODE_FILL_COLOR, Color.getHSBColor(1, (float) 0.5, (float) 0.5));
         	nodeView.setLockedValue(BasicVisualLexicon.NODE_SHAPE, NodeShapeVisualProperty.OCTAGON);
         	nodeView.setLockedValue(BasicVisualLexicon.NODE_SIZE, 70.0);
         	
         	temp+=1;        	
         }
+        
+        //CyEdge edge = CyTableUtil.getEdgesInState(arg0, arg1, arg2);
+        
+        
+        //network.getConnectingEdgeList(arg0, arg1, arg2)        		
 
         
         
         
         
-        int layer = (int)Math.sqrt((double)count2);
+        layer = (int)Math.sqrt((double)count2);
         layer /= 2;
         temp = 0;
         
         for (CyNode node : CyTableUtil.getNodesInState(network, "selected", false)){
         	nodeView = networkView.getNodeView(node);
         	
-        	double m = (temp*360/count2)*Math.PI/180;
-        	nodeView.setVisualProperty(BasicVisualLexicon.NODE_X_LOCATION, (140+r*(temp%layer)*layer)*Math.cos(m)+a);
-        	nodeView.setVisualProperty(BasicVisualLexicon.NODE_Y_LOCATION, (140+r*(temp%layer)*layer)*Math.sin(m)+b);
+        	m = (temp*360/count2)*Math.PI/180;
+        	nodeView.setVisualProperty(BasicVisualLexicon.NODE_X_LOCATION, (140+r*(count1-1)+r*(layer-(temp%layer))*layer)*Math.cos(m)+a);
+        	nodeView.setVisualProperty(BasicVisualLexicon.NODE_Y_LOCATION, (140+r*(count1-1)+r*(layer-(temp%layer))*layer)*Math.sin(m)+b);
         	
         	nodeView.setLockedValue(BasicVisualLexicon.NODE_FILL_COLOR, Color.getHSBColor((float) (temp/count2), 1, 1));
         	nodeView.setLockedValue(BasicVisualLexicon.NODE_SHAPE, NodeShapeVisualProperty.ELLIPSE);
